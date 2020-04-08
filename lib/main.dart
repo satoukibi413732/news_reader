e1aa41e28ebfc03457c873f0a2f1bae5e5547a44
zhoumengyu <zhoumengyu@sfere-elec.com>
2020-04-08 16:48:12 +0800

    '新增定位权限，文件访问权限，软件初始化时检查'

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:news_reader/application.dart';
 import 'package:news_reader/pages/home/home_page.dart';
 import 'package:news_reader/route/routes.dart';
+import 'package:news_reader/utils/permission_util.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   Router router = Router();
   Routes.configureRoutes(router);
   Application.router = router;
   runApp(MyApp());
+  PermissionUtil.getPermission([Permission.locationAlways, Permission.storage]);
 }
 
 class MyApp extends StatelessWidget {
