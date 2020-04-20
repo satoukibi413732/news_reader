8d8f07e94db8dab907c3f4a0c4fbcf4d7570de11
zhoumengyu <zhoumengyu@sfere-elec.com>
2020-04-20 16:56:34 +0800

    '新增webview组件'

---
+++ lib/widgets/browser.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class Browser extends StatelessWidget {
+  Browser({this.url, this.title});
+  final String url;
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: WebView(
+        initialUrl: url,
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
